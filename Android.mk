7d5e277c1485f607fb0b0ee3d5c0b455054e68ea
moongato <moongato@ubuntu>
2014-09-23 13:25:07 -0700

    Revert "Add optimization of Skia S32A_Opaque blitter"
    This reverts commit accc1d91c89b7e007613ee12c1ba65708035dae1.

--- Android.mk
+++ Android.mk
@@ -48,10 +48,6 @@ ifeq ($(ARCH_ARM_HAVE_NEON),true)
 	LOCAL_CFLAGS += -D__ARM_HAVE_NEON
 endif
 
-# Enable Neon assembler optimized version of S32A_Opaque_BlitRow32.
-# Overrides the intrinsic blitter below.
-LOCAL_CFLAGS += -DENABLE_OPTIMIZED_S32A_BLITTERS
-
 LOCAL_CFLAGS += -DDCT_IFAST_SUPPORTED
 
 # using freetype's embolden allows us to adjust fake bold settings at
@@ -521,7 +517,6 @@ ifeq ($(TARGET_ARCH),arm)
 
 ifeq ($(ARCH_ARM_HAVE_NEON),true)
 LOCAL_SRC_FILES += \
-	src/opts/S32A_Opaque_BlitRow32_neon.S \
 	src/opts/memset16_neon.S \
 	src/opts/memset32_neon.S \
 	src/opts/SkBitmapProcState_arm_neon.cpp \
